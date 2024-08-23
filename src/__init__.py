0498ccc225031938259acace32602c817fb367c5
David Chu <thedavidchu@gmail.com>
2024-08-23 18:07:32 -0400

    Add flask src

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+import datetime
+from pathlib import Path
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "base.html",
+        title="David's Webpage",
+        datetime=datetime,
+        # NOTE  Flask requires the images to be in the static directory.
+        favicon_path="static/favicon/android-chrome-512x512.png",
+    )
+
+
+# a simple page that says hello
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
